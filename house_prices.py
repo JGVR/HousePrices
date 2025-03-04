4326d7ce79907c6ebea70abac13dffd866ae2fb1
JGVR <juangabrielvasquez11@gmail.com>
2025-03-03 21:23:52 -0500

    applied gradient descent. Completed this small model. I created this repo to get some hands on experience with Linear Regression

--- house_prices.py
+++ house_prices.py
@@ -5,12 +5,28 @@ from models.house import House
 #build predicting model
 def f_wb(w, b, x):
     #amount of data points
-    n = x.shape[0]
-    predictions = np.zeros(n)
+    m = x.shape[0]
+    predictions = np.zeros(m)
 
-    for i in range(n):
+    for i in range(m):
         predictions[i] = w * x[i] + b
     return predictions
+
+# > gradient of the cost function 
+# > aJ(w,b)/aw, aj(w,b)/ab
+def gradient(w, b, x, y):
+    d_w = 0
+    d_b = 0
+    m = x.shape[0]
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        d_w += (f_wb - y[i]) * x[i]
+        d_b += (f_wb - y[i])
+
+    d_w = d_w / m
+    d_b = d_b / m
+    return d_w, d_b
 
 #cost function
 #formula > J(w,b) = 1/2m * sum (predictions[i] - expected result for i)^2
@@ -23,8 +39,28 @@ def mse(predictions, x, y):
 
     return 1/(2*m) * sum_of_predictions
 
+#gradient descent
+# > w = w - alpha * dJ(w,b)/dw
+# > b = b - alpha * dJ(w,b)/db
+def gradient_descent(w_init, b_init, alpha, iters, x, y):
+    w = w_init
+    b = b_init
+    history = []
+
+    for i in range(iters):
+        d_w, d_b = gradient(w, b, x, y)
+        w = w - alpha * d_w
+        b = b - alpha * d_b
+
+        #store the cost per prediction
+        predictions = f_wb(w, b, x)
+        cost = mse(predictions, x, y)
+        history.append(cost)
+
+    return w, b, history
+
 houses = []
-for i in range(1,6):
+for i in range(1,20):
     if i == 1:
         house = House(300 * i, 1.0 * i)
         houses.append(house)
@@ -37,16 +73,26 @@ for i in range(1,6):
 y_train = np.array([house.price for house in houses])
 x_train = np.array([house.sqtf for house in houses])
 
+#check gradient descent
+w, b, history = gradient_descent(0, 0, 0.001, 100000, x_train, y_train)
+print(f"value of weight: {w}")
+print(f"value of bias: {b}")
+
+#plot cost function
+plt.plot(history)
+plt.xlabel('Iterations')
+plt.ylabel('Cost')
+plt.title('Cost vs Iterations')
+plt.show()
+
 
 #plot pridection line
-w = 270
-b = 10
 predictions = f_wb(w, b, x_train)
 
 
 #check cost function
 cost = mse(predictions, x_train, y_train)
-print(cost)
+print(f"cost of mse: {cost}")
 
 
 #plot prediction limne
